5e73f7aa728d0e48ec7f5ef7fb1faf4c1a3fa1fe
ivan <ivanwonder@outlook.com>
2018-05-09 09:19:05 +0800

    focusing the window on new tab creation

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const communicationKey = 'set session cwd';
+
 exports.middleware = ({ dispatch }) => next => (action) => {
   const remote = require('electron').remote;
   const app = remote.app;
   switch (action.type) {
     case 'INIT':
       // listen the main process to set cwd before create a new tab.
-      window.rpc.on('set session cwd', (cwd) => {
+      window.rpc.on(communicationKey, (cwd) => {
         if (cwd) {
           dispatch({
             type: 'SESSION_SET_CWD',
@@ -26,9 +28,13 @@ exports.onApp = (app) => {
     const cwd = commandLine[1];
     const lastFocusedWindow = app.getLastFocusedWindow();
     // tell the render process to set the the tab's cwd before create a new one.
-    lastFocusedWindow.rpc.emit('set session cwd', cwd);
+    lastFocusedWindow.rpc.emit(communicationKey, cwd);
     // tell the render process to open a new tab.
     lastFocusedWindow.rpc.emit('termgroup add req');
+    
+    if (!lastFocusedWindow.isFocused()) {
+      lastFocusedWindow.focus();
+    }
   });
   if (isSecondInstance) {
     app.quit();
